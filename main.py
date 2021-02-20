fadedc3e44a62469dc195dd3af5a2ca8455830e5
Mara Hart <maralihart@gmail.com>
2021-02-20 12:37:45 -0500

    set up basic form look

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
-
-@app.route('/')
+    
+@app.route("/", methods=['GET', 'POST'])
 def index():
+  if request.method == "POST":
+    req = request.form
+    return redirect(request.url)
+
   return render_template('index.html')
